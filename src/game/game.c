d084c0c990c91de6fa9a2f0a99bf82531b7b29a7
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-06-12 11:30:02 +0200

    Setup fail.

--- src/game/game.c
+++ src/game/game.c
@@ -1,6 +1,16 @@
 #include <sio.h>
+#include <game.h>
 
 int testan() {
 	printf("asd");
 	return 2;
 }
+
+
+
+if (_TESTING_ == 1)
+{
+	printf("asdads\n");
+}
+
+
--- src/game/game.h
+++ src/game/game.h
@@ -1,6 +1,7 @@
 #ifndef _DTU_GAME_
 #define _DTU_GAME_
 
+
 int testan();
 
 #endif _DTU_GAME_
