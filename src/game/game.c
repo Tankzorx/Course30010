c2f15da41d6f45ae779126787bc31969722a4a61
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-06-12 15:12:56 +0200

    Went on compromise with timers, and dropped the idea of passing in an interrupt handler to our own function.

--- src/game/game.c
+++ src/game/game.c
@@ -1,8 +1,8 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>
 #include "game.h"
-#include "../hif/timer.h"
-#include "../api/menu.h"
+// #include "../hif/timer.h"
+// #include "../api/menu.h"
 
 int tickCounter = 0;
 
@@ -14,23 +14,22 @@ void interruptHandler() {
 
 void runGame() {
   int runMainLoop = 1;
-  initTimer(interruptHandler);
-  // DI();
-  // // Init Timer and interrupt settings.
-  // T0CTL = 0x19;
-  // // timer 0 byte registers set initial value:
-  // T0H = 0x0;
-  // T0L = 0x1;
-  // // Set reload stuff 5A00
-  // T0RH = 0x0A; // RH high
-  // T0RL = 0x00; // RL low
-
-  // // ENABLE TIMER INTERRUPT Set to level 3.
-  // IRQ0ENH |= 0x20;
-  // IRQ0ENL |= 0x20;
-  // // ENABLE TIMER
-  // SET_VECTOR(TIMER0, interruptHandler);
-  // EI();
+  //initTimer(interruptHandler);
+  DI();
+  // Init Timer and interrupt settings.
+  T0CTL = 0x19;
+  // timer 0 byte registers set initial value:
+  T0H = 0x0;
+  T0L = 0x1;
+  // Set reload stuff 5A00
+  T0RH = 0x0A; // RH high
+  T0RL = 0x00; // RL low
+  // ENABLE TIMER INTERRUPT Set to level 3.
+  IRQ0ENH |= 0x20;
+  IRQ0ENL |= 0x20;
+  // ENABLE TIMER
+  SET_VECTOR(TIMER0, interruptHandler);
+  EI();
 
   T0CTL |= 0x80;
   // addMenuItem("View Highscore");
--- src/main.c
+++ src/main.c
@@ -1,11 +1,33 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>             // special encore serial i/o routine
 
-#include "./game/game.h"
+// #include "./game/game.h"
+int tickCounter = 0;
+#pragma interrupt
+void interruptHandler() {
+  tickCounter++;
+}
 
 void main() {
 	init_uart(_UART0, _DEFFREQ, _DEFBAUD);  // set-up UART0 to 57600, 8n1
-	runGame();
+	// runGame();
+	DI();
+	// Init Timer and interrupt settings.
+	T0CTL = 0x19;
+	// timer 0 byte registers set initial value:
+	T0H = 0x0;
+	T0L = 0x1;
+	// Set reload stuff 5A00
+	T0RH = 0x0A; // RH high
+	T0RL = 0x00; // RL low
+	// ENABLE TIMER INTERRUPT Set to level 3.
+	IRQ0ENH |= 0x20;
+	IRQ0ENL |= 0x20;
+	// ENABLE TIMER
+	SET_VECTOR(TIMER0, interruptHandler);
+	EI();
+
+	T0CTL |= 0x80;
 	do {
 	} while (1);
 }
