b13909bdd603b992e77765b4770238892480a21b
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-06-12 11:02:45 +0200

    Creates files as a result of block diagram.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>             // special encore serial i/o routines
 
+#define __TESTING__ 1
 
 void main() {
 	init_uart(_UART0, _DEFFREQ, _DEFBAUD);  // set-up UART0 to 57600, 8n1
+	// runGame()
 	do {
 	} while (1);
 }
